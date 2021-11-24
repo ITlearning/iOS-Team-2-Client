582068502e7568ce18e73ec3857885e8d55f67cb
ITlearning <yo7504@naver.com>
2021-11-25 03:36:11 +0900

    #27 - 이력관리 뷰 셀 제작

---
+++ Budi/Budi/Source/Login/ReuseViews/HistoryTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryTypeView.swift
+//  Budi
+//
+//  Created by 인병윤 on 2021/11/25.
+//
+
+import UIKit
+
+class HistoryTypeView: UIView {
+
+    @IBOutlet weak var plusButton: UIButton!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
